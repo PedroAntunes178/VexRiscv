3b24b30dc5471f30a6766c7c106e84aaf8ad45e6
Pedro Antunes <pedronmantunes@gmail.com>
2022-03-16 15:16:03 +0000

    no C support

--- src/main/scala/vexriscv/demo/Linux.scala
+++ src/main/scala/vexriscv/demo/Linux.scala
@@ -32,7 +32,7 @@ object LinuxGen {
       plugins = List(
         new IBusCachedPlugin(
           resetVector = 0x80000000l,
-          compressedGen = true,
+          compressedGen = false,
           prediction = STATIC,
           injectorStage = false,
           config = InstructionCacheConfig(
